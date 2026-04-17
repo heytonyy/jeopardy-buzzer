59368d70988277562c5db707e25c2a3b6b4812e1
heytonyy <heytonyyy3@gmail.com>
2026-04-17 07:28:59 -0700

    updated api and socket url for deploymen

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SERVER_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const SERVER_URL = import.meta.env.DEV ? 'http://localhost:3001' : window.location.origin;
 
 let teacherSocket = null;
 let participantSocket = null;
